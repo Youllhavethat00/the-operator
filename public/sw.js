3936b9db894e48408feab9eacacb2a22aa58312d
Your Name <you@example.com>
2026-01-01 02:59:38 -0500

    Add service worker and manifest.json for PWA

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'the-operator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(() => console.log('Cache initialization failed'))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => new Response('Offline'))
+  );
+});
